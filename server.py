048e2b53890c7ae7f025d386e0ded2f1d4b24980
Chirag V <chirag42012@gmail.com>
2023-08-24 12:27:20 +0530

    coded add function.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -9,7 +9,11 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    print("triggered")
+    data = request.get_json()
+    return {
+        "result": data["first"] + data["second"]
+    }
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
